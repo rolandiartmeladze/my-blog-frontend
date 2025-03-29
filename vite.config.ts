db29cd7f4bf9bb97b6d8893a26d0fa262e71ccdc
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-03-29 05:10:35 +0400

    fix small detalies

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
-import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),tailwindcss(), eslintPlugin()],
+  plugins: [vue(),tailwindcss()],
 })
