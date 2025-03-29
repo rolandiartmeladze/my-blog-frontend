77b654b882453a6cc51352d529fadbf35f3a8437
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-03-29 05:07:06 +0400

    uptate format code

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,7 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <h1 class="fixed top-0 max-w-full text-center">
-    this is Blog header
-  </h1>
-
+  <h1 class="fixed top-0 max-w-full text-center">this is Blog header</h1>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),tailwindcss(),],
+  plugins: [vue(),tailwindcss(), eslintPlugin()],
 })
