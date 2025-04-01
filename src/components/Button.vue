4486134201d02174e99a7fdbba3087e4ae32ed75
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-04-02 03:24:50 +0400

    update components

---
+++ src/components/Button.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <button class="bg-accent text-white py-2 px-4 rounded-md">
+    Contact
+  </button>
+</template>
+
+<style scoped></style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from './Button.vue';
+</script>
 
 <template>
-  <header class="w-full h-14 flex items-center justify-between px-6 py-10 shadow-md">
+  <header class="w-full bg-secondary h-14 flex items-center justify-between px-6 py-10 shadow-md">
     <h1 class="text-lg font-semibold">RA Blog</h1>
-    <button>Contact</button>
+    <Button />
   </header>
 </template>
 
