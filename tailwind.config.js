09a165fa19f18d1d275152fc29b3e827c01ecf5d
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-04-02 00:57:41 +0400

    update color structure with project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
-module.exports = {
-    content: [
-      "./src/**/*.{html,js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {},
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E293B",
+        secondary: "#64748B",
+        accent: "#E11D48",
+        background: "#F8FAFC",
+        text: "#1E293B",
+        border: "#CBD5E1",
+        success: "#22C55E",
+        warning: "#EAB308",
+        error: "#DC2626",
+      },
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [],
+};
