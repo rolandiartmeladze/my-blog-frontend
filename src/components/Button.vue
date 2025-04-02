4cec060da8c2e5513d6fbdf9381ba2a73767dabf
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-04-02 23:19:24 +0400

    format code

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -2,29 +2,31 @@
 import { computed } from 'vue';
 
 const props = defineProps<{
-  variant?: "primary" | "secondary" | "accent" | "outline";
-  type?: "button" | "submit";
-}>();;
+  variant?: 'primary' | 'secondary' | 'accent' | 'outline';
+  type?: 'button' | 'submit';
+}>();
 
 const buttonClasses = computed(() => {
-
   switch (props.variant) {
-    case "primary":
-      return "bg-primary text-white hover:bg-opacity-80";
-    case "secondary":
-      return "bg-secondary text-white hover:bg-secondary";
-    case "accent":
-      return "bg-accent text-white hover:bg-secondary";
-    case "outline":
-      return "border border-border text-text hover:bg-border";
+    case 'primary':
+      return 'bg-primary text-white hover:bg-opacity-80';
+    case 'secondary':
+      return 'bg-secondary text-white hover:bg-secondary';
+    case 'accent':
+      return 'bg-accent text-white hover:bg-secondary';
+    case 'outline':
+      return 'border border-border text-text hover:bg-border';
     default:
-      return "bg-primary text-white";
+      return 'bg-primary text-white';
   }
 });
 </script>
 
 <template>
-  <button :type="type || 'button'" :class="`px-4 py-2 rounded-md transition ${buttonClasses}`">
+  <button
+    :type="type || 'button'"
+    :class="`px-4 py-2 rounded-md transition ${buttonClasses}`"
+  >
     <slot />
   </button>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,9 @@ import Navigation from './Navigation.vue';
 </script>
 
 <template>
-  <header class="w-full bg-secondary h-14 flex items-center justify-between px-6 py-10 shadow-md">
+  <header
+    class="w-full bg-secondary h-14 flex items-center justify-between px-6 py-10 shadow-md"
+  >
     <h1 class="text-lg font-semibold">RA Blog</h1>
     <Navigation />
   </header>
