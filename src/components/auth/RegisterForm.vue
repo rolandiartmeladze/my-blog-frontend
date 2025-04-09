e694ae550be8c9ca1f03fbff6b270de6e136bc10
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-04-10 03:01:43 +0400

    add register.vue

---
+++ src/components/auth/RegisterForm.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Button from '../ui/Button.vue';
+</script>
+
+<template>
+  <form class="flex flex-col gap-4">
+    <h1 class="text-2xl font-bold">Register</h1>
+
+    <input type="email" placeholder="Email" class="border px-3 py-2 rounded" />
+    <input type="password" placeholder="Password" class="border px-3 py-2 rounded" />
+
+    <Button type="submit" variant="primary" class="w-full">
+      Register
+    </Button>
+  </form>
+</template>
