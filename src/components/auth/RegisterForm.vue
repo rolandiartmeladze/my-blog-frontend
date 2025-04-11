6dbee75b6f10e46b96136f8c8e60d030d2d32c2f
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-04-11 14:49:05 +0400

    update registr vue UI

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -5,13 +5,43 @@ import Button from '../ui/Button.vue';
 <template>
   <form class="flex flex-col gap-4">
     <h1 class="text-2xl font-bold">Register</h1>
+    <div class="flex flex-col gap-1">
+    <label for="name">You Name</label>
 
-    <input type="email" placeholder="Email" class="border px-3 py-2 rounded" />
+    <input
+      type="text"
+      placeholder="Name"
+      class="border px-3 py-2 rounded"
+    />
+</div>
+
+
+    <div class="flex flex-col gap-1">
+      <label for="email">Email</label>
+
+    <input type="email" placeholder="Email" class="border px-3 py-2 rounded" />    
+  </div>
+
+  <div class="flex flex-col gap-1">
+    <label for="password">Password</label>
+
     <input
       type="password"
       placeholder="Password"
       class="border px-3 py-2 rounded"
     />
+</div>
+
+<div class="flex flex-col gap-1">
+    <label for="Rep Password">Rep Password</label>
+
+    <input
+      type="password"
+      placeholder="Rep Password"
+      class="border px-3 py-2 rounded"
+    />
+</div>
+
 
     <Button type="submit" variant="primary" class="w-full"> Register </Button>
   </form>
