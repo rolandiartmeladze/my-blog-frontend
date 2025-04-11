f3b4b8a3f9454cdd73c1f065b3484c0c6ee10db2
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-04-11 16:47:26 +0400

    update login form

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -25,7 +25,7 @@ const handleSubmit = async () => {
 
     const response = await axios.post(
       'http://localhost:8000/api/login',
-      form,
+      form.value,
       {
         headers: {
           'Content-Type': 'application/json',
@@ -58,42 +58,27 @@ const handleSubmit = async () => {
 
     <div class="flex flex-col gap-1">
       <label for="email">Email</label>
-      <input
-        v-model="form.email"
-        type="email"
-        id="email"
-        placeholder="Enter your email"
-        required
-        class="border px-3 py-2 rounded"
-      />
+      <input v-model="form.email" type="email" id="email" placeholder="Enter your email" required
+        class="border px-3 py-2 rounded" />
     </div>
 
     <div class="flex flex-col gap-1">
       <label for="password">Password</label>
-      <input
-        v-model="form.password"
-        type="password"
-        id="password"
-        placeholder="Enter your password"
-        required
-        class="border px-3 py-2 rounded"
-      />
+      <input v-model="form.password" type="password" id="password" placeholder="Enter your password" required
+        class="border px-3 py-2 rounded" />
     </div>
 
-    <div class="flex gap-3 flex-col lg:flex-row justify-between pr-6" >
+    <div class="flex gap-3 flex-col lg:flex-row justify-between pr-6">
       <div class="flex justify-center items-center gap-2 mt-2">
 
-      
-    <input 
-      type="checkbox" 
-      id="remember" 
-      class="mr-2 border-primary checked:bg-primary checked:border-primary focus:ring-primary" 
-    />
-    <label for="remember" class="text-sm">Remember me</label>
-</div>
-    <div class="flex justify-between mt-2 lg:flex-col lg:gap-2 lg:text-center">
-      <a href="#" class="text-sm text-accent hover:underline">Forgot password?</a>
-      <a href="#" class="text-sm text-accent hover:underline">Create an account</a>
+
+        <input type="checkbox" id="remember"
+          class="mr-2 border-primary checked:bg-primary checked:border-primary focus:ring-primary" />
+        <label for="remember" class="text-sm">Remember me</label>
+      </div>
+      <div class="flex justify-between mt-2 lg:flex-col lg:gap-2 lg:text-center">
+        <a href="#" class="text-sm text-accent hover:underline">Forgot password?</a>
+        <a href="#" class="text-sm text-accent hover:underline">Create an account</a>
       </div>
     </div>
 
@@ -106,11 +91,11 @@ const handleSubmit = async () => {
         Login With: <samp class="text-xs text-accent ">comming soon..</samp>
       </h1>
       <div class="flex gap-4 mt-2 w-full justify-center">
-      <SvgIcon name="gmail" class="size-10 cursor-pointer hover:text-accent" />
-      <SvgIcon name="github" class="size-10 cursor-pointer hover:text-accent" />
+        <SvgIcon name="gmail" class="size-10 cursor-pointer hover:text-accent" />
+        <SvgIcon name="github" class="size-10 cursor-pointer hover:text-accent" />
       </div>
-      </div>
-      
+    </div>
+
 
 
   </form>
