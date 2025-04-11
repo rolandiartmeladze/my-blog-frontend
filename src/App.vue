18b5233ef278759cf668d417ad120e7813b2961b
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-04-11 16:46:55 +0400

    feat footer.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import Header from './components/Header.vue';
+import Footer from './components/footer/Footer.vue';
 </script>
 
 <template>
+
   <Header />
   <RouterView />
+  <Footer />
+
 </template>
 
 <style scoped></style>
---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+import Icons from '../Icons';
+</script>
+
+<template>
+  <footer class="w-full absolute bottom-13 bg-secondary h-16 flex items-center justify-between px-6 py-4 shadow-lg mt-3 lg:fixed lg:bottom-0">
+    <h1 class="text-lg font-semibold text-white hover:text-yellow-300 transition duration-300">
+      © Roland Artmeladze
+    </h1>
+    <div class="flex space-x-6">
+        <div>
+      <a
+        href="https://github.com/rolandiartmeladze"
+        target="_blank"
+        class="text-yellow-300 hover:text-white transition duration-300 flex items-center space-x-2"
+      >                  
+      <Icons name="github" class="text-white hover:text-yellow-300 transition duration-300" />
+
+        <span>GitHub</span>
+      </a>
+  
+        </div>
+      <a
+        href="https://linkedin.com/in/rolandi-artmeladze"
+        target="_blank"
+        class="text-yellow-300 hover:text-white transition duration-300 flex items-center space-x-2"
+      >
+      
+        <span>LinkedIn</span>
+      </a>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  border-top: 2px solid #0e131d;
+  font-family: 'Poppins', sans-serif;
+}
+
+footer a {
+  font-size: 1rem;
+  font-family: 'Roboto', sans-serif;
+}
+
+footer a i {
+  font-size: 1.2rem;
+}
+</style>
