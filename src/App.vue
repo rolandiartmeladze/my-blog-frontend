abd4efee08fe2dda461da50d728588141635b01d
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-04-30 17:50:44 +0400

    feat: restructure App.vue layout and integrate ElementPlus in main.ts

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,16 @@ import Footer from './components/footer/Footer.vue';
 </script>
 
 <template>
-
-  <Header />
-  <RouterView />
-  <Footer />
-
+  <div class="common-layout min-h-screen flex flex-col justify-between">
+    <el-container>
+      <el-header><Header /></el-header>
+      <el-main><RouterView /></el-main>
+      <el-footer><Footer /></el-footer>
+    </el-container>
+  </div>
 </template>
 
+  
+  
+  
 <style scoped></style>
