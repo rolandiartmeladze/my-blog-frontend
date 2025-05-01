803095dc23c59c89d490bd61f321f1312a252f05
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-05-02 02:55:34 +0400

    fix: restructure HomeView template for improved layout and consistency

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,7 +10,13 @@ const JoinAs = () => {
 </script>
 
 <template>
-  <Button  type="submit" variant='accent' @click="JoinAs"  class="m-5">
+  <div 
+    class="flex flex-col bg-secondary items-start justify-start h-1/4 mt-4 p-6 rounded-lg shadow-md bg-desktop">
+    <h1 class="text-3xl lg:text-4xl text-accent font-bold mb-4">Welcome to Our Blog</h1>
+    <p class="text-lg text-primary lg:text-secondary mb-8 px-2 rounded-b-md border-b-2 border-b-accent">Discover inspiring stories. Join us!</p>
+
+  <Button  type="submit" variant='accent' @click="JoinAs"  class="m-5 p-5 hower:bg-primary">
     Join As
   </Button>
+  </div>
 </template>
