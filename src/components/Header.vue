7ae71317e0cc66aa10cf652e84c2e5ff3a2d8630
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-05-01 13:24:49 +0400

    fix: update Header and PublicNavigation components for improved styling and layout consistency

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,19 @@
 <script setup lang="ts">
 import Icons from './Icons';
 import PublicNavigation from './navigation/PublicNavigation.vue';
-import PrivateNavigation from './navigation/PrivateNavigation.vue';
 </script>
 
 <template>
   <header
-    class="w-full bg-secondary h-14 flex items-center justify-between px-6 py-10 shadow-md"
+    class="w-full h-14 flex items-center justify-between mb-4 px-6 py-10 shadow-accent shadow-sm rounded-b-md lg:rounded-b-xl lg:shadow-lg"
   >
-    <h1 class="text-lg font-semibold">
+    <h1 class="text-lg text-accent font-semibold hover:text-secondary transition duration-300">
       <RouterLink :to="'/'"> RA Blog </RouterLink>
     </h1>
     <div>
-      <Icons name="menu" class="visible size-10 cursor-pointer hover:text-accent lg:hidden" />
+      <Icons name="menu" class="visible size-10 cursor-pointer text-secondary hover:text-accent lg:hidden" />
     </div>
     <PublicNavigation />
-    <PrivateNavigation />
   </header>
 </template>
 
