f8839c344aa6088467a36b170e0253e3d695b10a
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-05-01 13:25:28 +0400

    fix: enhance layout and styling in App.vue for improved consistency

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
 import Header from './components/Header.vue';
-import Footer from './components/footer/Footer.vue';
+import Footer from './components/footer/Footer.vue'; 
+import PrivateNavigtion from './components/navigation/PrivateNavigation.vue'
 </script>
 
 <template>
-  <div class="common-layout min-h-screen flex flex-col justify-between">
-    <el-container>
+  <div class="common-layout bg-current min-h-screen flex flex-col justify-between items-center">
+    <el-container class="w-full max-w-[1440px]">
       <el-header><Header /></el-header>
-      <el-main><RouterView /></el-main>
-      <el-footer><Footer /></el-footer>
+      <el-main class="relative">
+        <RouterView />
+        <PrivateNavigtion class="fixed bottom-0 left-0 w-full md:static md:w-auto" />
+      </el-main>
+      <el-footer class="flex"><Footer /></el-footer>
     </el-container>
   </div>
 </template>
