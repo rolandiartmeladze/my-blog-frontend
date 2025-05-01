217095556effd31c81da4b9ee34ce222ae98a8cd
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-05-02 03:00:31 +0400

    fix: update shadow style in header for improved visual consistency

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@ import PublicNavigation from './navigation/PublicNavigation.vue';
 
 <template>
   <header
-    class="w-full h-14 flex items-center justify-between mb-4 px-6 py-10 shadow-accent shadow-sm rounded-b-md lg:rounded-b-xl lg:shadow-lg"
+    class="w-full h-14 flex items-center justify-between mb-4 px-6 py-10 shadow-accent shadow-sm rounded-b-md lg:rounded-b-xl lg:shadow-2xl"
   >
     <h1 class="text-lg text-accent font-semibold hover:text-secondary transition duration-300">
       <RouterLink :to="'/'"> RA Blog </RouterLink>
