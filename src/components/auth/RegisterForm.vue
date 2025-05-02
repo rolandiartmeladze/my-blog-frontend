a45e70946cd004af10cbe071357f65f296ec3b9b
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-05-02 15:03:55 +0400

    fix: correct typo in label for name input in RegisterForm

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,47 +1,28 @@
 <script setup lang="ts">
 import Button from '../ui/Button.vue';
+import Input from '../ui/Input.vue';
+import { ref } from 'vue';
+
+const form = ref({
+  name: '',
+  email: '',
+  password: '',
+});
+
+const handleSubmit = async () => {
+  console.log('Form submitted:', form.value);
+};
 </script>
 
 <template>
-  <form class="flex flex-col gap-4">
+  <form  @submit.prevent="handleSubmit" class="flex flex-col gap-4">
     <h1 class="text-2xl font-bold">Register</h1>
-    <div class="flex flex-col gap-1">
-    <label for="name">You Name</label>
 
-    <input
-      type="text"
-      placeholder="Name"
-      class="border px-3 py-2 rounded"
-    />
-</div>
-
-
-    <div class="flex flex-col gap-1">
-      <label for="email">Email</label>
-
-    <input type="email" placeholder="Email" class="border px-3 py-2 rounded" />    
-  </div>
-
-  <div class="flex flex-col gap-1">
-    <label for="password">Password</label>
-
-    <input
-      type="password"
-      placeholder="Password"
-      class="border px-3 py-2 rounded"
-    />
-</div>
-
-<div class="flex flex-col gap-1">
-    <label for="Rep Password">Rep Password</label>
-
-    <input
-      type="password"
-      placeholder="Rep Password"
-      class="border px-3 py-2 rounded"
-    />
-</div>
-
+    <Input id="name" label="Yor Name" type="text" placeholder="Enter your name" v-model="form.name" />
+    <Input id="email" label="Email" type="email" placeholder="Enter your email" v-model="form.email" />
+    <Input id="password" label="Password" type="password" placeholder="Enter your password" v-model="form.password" />
+    <Input id="reppassword" label="Rep Password" type="password" placeholder="Enter rep password"
+      v-model="form.password" />
 
     <Button type="submit" variant="primary" class="w-full"> Register </Button>
   </form>
