cfa142149e99d0fac10357efd36baef0e965185a
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-05-02 14:38:56 +0400

    feat: add WelcomeSection component to HomeView for enhanced user engagement

---
+++ src/components/WelcomeSection.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Button from '../components/ui/Button.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const handleJoin = () => {
+  router.push('/auth');
+};
+</script>
+
+<template>
+  <div 
+    class="flex flex-col bg-secondary items-start justify-start h-1/4 mt-4 p-6 rounded-lg shadow-md bg-desktop">
+    <h1 class="text-3xl lg:text-4xl text-accent font-bold mb-4">Welcome to Our Blog</h1>
+    <p class="text-lg text-white lg:text-secondary mb-8 px-2 rounded-b-md border-b-2 border-b-accent">
+      Discover inspiring stories. Join us!
+    </p>
+
+    <Button type="submit" variant="accent" @click="handleJoin" class="m-5 p-5 hover:bg-primary">
+      Join As
+    </Button>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,7 @@
 <script setup lang="ts">
-import Button from '../components/ui/Button.vue';
-import { useRouter } from 'vue-router';
-
-const router = useRouter();
-const JoinAs = () => {
-  router.push('/auth');
-};
-
+import WelcomeSection from '../components/WelcomeSection.vue';
 </script>
 
 <template>
-  <div 
-    class="flex flex-col bg-secondary items-start justify-start h-1/4 mt-4 p-6 rounded-lg shadow-md bg-desktop">
-    <h1 class="text-3xl lg:text-4xl text-accent font-bold mb-4">Welcome to Our Blog</h1>
-    <p class="text-lg text-primary lg:text-secondary mb-8 px-2 rounded-b-md border-b-2 border-b-accent">Discover inspiring stories. Join us!</p>
-
-  <Button  type="submit" variant='accent' @click="JoinAs"  class="m-5 p-5 hower:bg-primary">
-    Join As
-  </Button>
-  </div>
+  <WelcomeSection />
 </template>
