1ea1e6117cfa6400b29b5a6eb724055532583d3b
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-05-05 02:30:10 +0400

    feat: add DashboardView component with user fetching functionality

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { fetchUser } from '../services/fetchUser.ts';
+
+const users = ref([]); // ინიციალიზაცია ცარიელი მასივით
+const errorMessage = ref<string | null>(null);
+
+onMounted(async () => {
+  try {
+    const response = await fetchUser();
+    users.value = response;
+    console.log('Users:', users.value);
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    errorMessage.value = 'Failed to load user data.';
+  }
+});
+</script>
+
+<template>
+  <div class="flex flex-col items-start justify-start mt-4 p-11 rounded-lg bg-gray-100">  
+    <h1 class="text-3xl text-accent my-6">Dashboard</h1>
+
+    <h2 v-if="users.length > 0">Welcome, You are on Board, hear is users:</h2>
+    <p v-else-if="errorMessage" class="text-red-500">{{ errorMessage }}</p>    
+    <p v-else>Loading...</p>
+
+    <div v-for="user in users" :key="user.id">
+      <ul class="flex"><li>{{ user.name }}</li>
+      <li>{{ user.email }}</li></ul>
+</div>    
+  </div>
+</template>
