f69ccbd524bcd23daa2cb09256b9b3b5916d92b3
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-05-05 02:28:38 +0400

    feat: implement authentication handling in router and main application

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import router from './router';
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+
+import { useAuth } from './auth.ts'
+
+const auth = useAuth();
 
 const app = createApp(App);
+
 app.use(router);
-app.use(ElementPlus)
+app.provide('auth', auth);
+app.use(ElementPlus);
+
 app.mount('#app');
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import { useAuth } from '../auth.ts';
 import AuthView from '../views/AuthView.vue';
 import HomeView from '../views/HomeView.vue';
+import DashboardView from '../views/DashboardView.vue';
 import AboutView from '../views/AboutView.vue';
 import NewsView from '../views/NewsView.vue';
 import PostsView from '../views/PostsView.vue';
@@ -10,6 +12,11 @@ import ContactView from '../views/ContactView.vue';
 const routes = [
   { path: '/auth', name: 'auth', component: AuthView },
   { path: '/', name: 'home', component: HomeView },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: DashboardView
+  },
   { path: '/about', name: 'about', component: AboutView },
   { path: '/news', name: 'news', component: NewsView },
   { path: '/posts', name: 'posts', component: PostsView },
@@ -19,6 +26,15 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, _, next) => {
+  const { isAuthenticated } = useAuth();
+  if (to.meta.auth && !isAuthenticated.value) {
+    next('/auth');
+  } else {
+    next();
+  }
 });
 
 export default router;
