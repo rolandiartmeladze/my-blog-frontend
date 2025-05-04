0df5ea87af89cd13d7dd23a7d6b91d026573af53
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-05-05 03:31:17 +0400

    feat: implement conditional rendering for WelcomeSection and Dashboard based on authentication status

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
 import WelcomeSection from '../components/WelcomeSection.vue';
+import Dashboard from '../views/DashboardView.vue';
+import { useAuth } from '../auth';
+
+const { isAuthenticated } = useAuth();
 </script>
 
 <template>
-  <WelcomeSection />
+  <WelcomeSection v-if="!isAuthenticated" />
+  <Dashboard v-if="isAuthenticated" />
 </template>
