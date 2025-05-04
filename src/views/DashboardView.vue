e43ea1e49ce5ede0b56c71eb83148be6d93311bb
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-05-05 03:30:57 +0400

    feat: enhance dashboard with user authentication and logout functionality

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
 import { fetchUser } from '../services/fetchUser.ts';
+import Button from '../components/ui/Button.vue';
+import { useAuth } from '../auth';
+import router from '../router/index.ts';
 
 interface User {
   id: number;
   name: string;
   email: string;
 }
+
+const { logout, isAuthenticated } = useAuth();
 
 const users = ref<User[]>([]); 
 const errorMessage = ref<string | null>(null);
@@ -20,11 +25,17 @@ onMounted(async () => {
     console.error('Error fetching users:', error);
     errorMessage.value = 'Failed to load user data.';
   }
+  
 });
+
+if(!isAuthenticated.value){
+  router.push('/auth');
+}
+
 </script>
 
 <template>
-  <div class="flex flex-col items-start justify-start mt-4 p-11 rounded-lg bg-gray-100">  
+  <div v-if="isAuthenticated"  class="flex flex-col items-start justify-start mt-4 p-11 rounded-lg bg-gray-100">  
     <h1 class="text-3xl text-accent my-6">Dashboard</h1>
 
     <h2 v-if="users.length > 0">Welcome, You are on Board, hear is users:</h2>
@@ -35,5 +46,9 @@ onMounted(async () => {
       <ul class="flex"><li>{{ user.name }}</li>
       <li>{{ user.email }}</li></ul>
 </div>    
+
+<Button variant="primary" class="mt-4" @click="logout">
+  LogOut
+</Button>
   </div>
 </template>
